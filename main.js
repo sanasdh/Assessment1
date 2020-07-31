69fe8e9e36e612195fd5b1d4925cc096af78b2b1
sanasdh <sanasdh@gmail.com>
2020-07-31 19:14:20 -0400

    done!

--- main.js
+++ main.js
@@ -1,14 +1,22 @@
+/*----- constants -----*/
+
+/*----- cached element references -----*/
 let value=document.querySelector(".value");
 let neg=document.querySelector(".neg");
 let positive=document.querySelector(".positive");
 let input=document.querySelector("input");
 let countDown=document.querySelector(".countDown")
+
+/*----- app's state (variables) -----*/
 value.innerHTML=0;
 let newValue=0;
+
+/*----- event listeners -----*/
 
 neg.addEventListener("click", negFunc);
 positive.addEventListener("click", positiveFun);
 
+/*----- functions -----*/
 function negFunc(evt){
   newValue=newValue-(input.value);
   countDown.textContent="";
